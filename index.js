54cef7d361e04eb1e4c11d525da9d0067760b20c
Collin McCoy <collinmccoy17@gmail.com>
2017-04-24 12:00:38 -0700

    array edits

---
+++ index.js
@@ -0,0 +1,30 @@
+alert('hi');
+var planets = [
+  ["Sun", 27.9],
+  ["Mercury", 0.377],
+  ["Venus", 0.9032],
+  ["Earth", 1],
+  ["Moon", 0.1655],
+  ["Mars", 0.3895],
+  ["Jupiter", 2.64],
+  ["Saturn", 1.139],
+  ["Uranus", 0.917],
+  ["Neptune", 1.148],
+  ["Pluto", 0.059],
+];
+var planetName = document.createElement.onclick(planets[0])
+for (x = 0; x <= planets.length; x++) {
+  result[x] = f(a[x]);
+
+document.getElementById('calculateWeight').onclick = function handleClickEvent(ev) {
+    alert('Hello!');
+    function calculateWeight() {
+      var userWeight = document.getElementById('userWeight').value;
+      }
+
+      var gravity = e.options[e.selectedIndex].value;
+      alert('On ' + planetName + ' you would weigh ' + userWeight * gravity);
+
+    }
+    calculateWeight();
+    alert('On ' + planetName + ' you would weigh ' + userWeight * gravity);
